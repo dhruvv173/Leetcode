1510e5c729a01c3a3413e9c7f8b1e29cfe12b19c
Dhruv Parmar <dhruvv173@gmail.com>
2023-05-27 20:21:26 +0530

    Graphs #2

---
+++ Graphs/BFS.py
@@ -0,0 +1,20 @@
+#Solution
+from collections import deque
+class Solution:
+#Function to return Breadth First Traversal of given graph.
+def bfsOfGraph(self, V: int, adj: List[List[int]]) -> List[int]:
+    # code here
+	vis = [0] * V
+	vis[0] = 1
+	q = deque()
+	q.append(0)
+	bfs = []    
+	while q:
+	  node = q.popleft()  # get the topmost element in the queue
+    bfs.append(node)
+    for it in adj[node]:  # traverse for all its neighbours
+        if not vis[it]:  # if the neighbour has not been visited
+            vis[it] = 1  # mark as visited
+            q.append(it)  # store in the queue
+
+    return bfs
---
+++ Graphs/DFS.py
@@ -0,0 +1,19 @@
+class Solution:
+    #Function to return a list containing the DFS traversal of the graph.
+    def dfs(self, node, adj, vis, dfs):
+        vis[node] = 1
+        dfs.append(node)
+        # traverse all its neighbors
+        for it in adj[node]:
+            # if the neighbor is not visited
+            if not vis[it]:
+                self.dfs(it, adj, vis, dfs)
+
+    def dfsOfGraph(self, V, adj):
+        vis = [0] * V
+        start = 0
+        # create a list to store dfs
+        dfs = []
+        # call dfs for starting node
+        self.dfs(start, adj, vis, dfs)
+        return dfs

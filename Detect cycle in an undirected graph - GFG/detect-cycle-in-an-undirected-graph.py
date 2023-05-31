105d7a2c0564aabf26b64c38a579bdc71609b738
Dhruv <79097544+dhruvv173@users.noreply.github.com>
2023-05-31 19:43:50 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.py
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.py
@@ -1,32 +1,24 @@
 from typing import List
-from collections import deque
 class Solution:
-    #Function to detect cycle in an undirected graph.
-    #Cycle detection usig BFS
-	def isCycle(self, V: int, adj: List[List[int]]) -> bool:
-		#Code here
-		vis = [0] * V
-		for i in range(V):
-		    if not vis[i]:
-		        if self.detect(i, adj, vis):
-		            return True
-		return False
-    
-    def detect(self, src, adj, vis):
-        vis[src] = 1
-        queue = deque([(src, -1)])
-        while queue:
-            node = queue[0][0]
-            parent = queue[0][1]
-            queue.popleft()
-            
-            for adjacentNode in adj[node]:
-                if not vis[adjacentNode]:
-                    vis[adjacentNode] = 1
-                    queue.append((adjacentNode, node))
-                elif parent != adjacentNode:
+    def isCycle(self, V: int, adj: List[List[int]]) -> bool:
+        vis = [0] * V
+        for i in range(V):
+            if vis[i] == 0:
+                if self.dfs(i, -1, vis, adj) == True:
                     return True
         return False
+
+    def dfs(self, node, parent, vis, adj):
+        vis[node] = 1
+        for adjacentNode in adj[node]:
+            if not vis[adjacentNode]:
+                if self.dfs(adjacentNode, node, vis, adj) == True:
+                    return True
+            elif vis[adjacentNode] and adjacentNode != parent:
+                return True
+        return False
+
+
 
 #{ 
  # Driver Code Starts

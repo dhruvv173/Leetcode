59cb2a130c485db0a31e0bca7c4e9a7a98c611fe
Dhruv <79097544+dhruvv173@users.noreply.github.com>
2023-06-26 12:43:03 +0530

    Added solution - LeetHub

---
+++ Kth smallest element - GFG/kth-smallest-element.py
@@ -0,0 +1,46 @@
+#User function Template for python3
+from heapq import heappop, heappush, heapify
+class Solution:
+    def kthSmallest(self,arr, l, r, k):
+        '''
+        arr : given array
+        l : starting index of the array i.e 0
+        r : ending index of the array i.e size-1
+        k : find kth smallest element and return using this function
+        '''
+        maxheap = []
+        heapify(maxheap)
+        for i in arr:
+            heappush(maxheap, -1 * i)
+            if len(maxheap) > k:
+                heappop(maxheap)
+        return maxheap[0] * -1
+
+
+# def getKSmallest(arr, k):
+#     maxheap = []
+#     heapify(maxheap)
+
+#     for i in arr:
+#         heappush(maxheap, -1 * i)
+#         if len(maxheap) > k:
+#             heappop(maxheap)
+    
+#     return maxheap[0]  *  -1
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__ == '__main__': 
+    import random 
+    t=int(input())
+    for tcs in range(t):
+        n=int(input())
+        arr=list(map(int,input().strip().split()))
+        k=int(input())
+        ob=Solution()
+        print(ob.kthSmallest(arr, 0, n-1, k))
+        
+# } Driver Code Ends

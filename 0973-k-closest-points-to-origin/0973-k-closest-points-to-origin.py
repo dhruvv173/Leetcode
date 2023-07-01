0f11c42c006e7bbd4cafbc9c4c26c9a0c867326a
Dhruv <79097544+dhruvv173@users.noreply.github.com>
2023-07-01 15:01:48 +0530

    Time: 801 ms (73.70%), Space: 22.3 MB (94.33%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -6,13 +6,13 @@ class Solution:
         heapify(maxHeap)
         for point in points:
             x,y = point[0], point[1]
-            dist = (x ** 2 + y ** 2)
-            heappush(maxHeap, (-dist, point))
+            #calc distance for both the points from origin
+            dist = (x ** 2 + y ** 2) #can just store the dist instead of sqrt(dist)
+            #append the dist first so the heap is sorted by dist
+            heappush(maxHeap, (-dist, point))  # -dist cus using maxHeap
 
             if len(maxHeap) > k:
                 heappop(maxHeap)
-        
         for _, point in maxHeap:
             res.append(point)
-
         return res

a82101e2ac09610cabce3ed4a433a8b2a001a718
Dhruv <79097544+dhruvv173@users.noreply.github.com>
2023-08-20 23:42:35 +0530

    Time: 1406 ms (48.11%), Space: 33.2 MB (46.69%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        # Brute force approach O(n^2)
+        # ans = []
+        # for i in range(len(nums)-k+1):
+        #     ans.append(max(nums[i:i+k]))
+        # return ans
+        # not optimal
+        i = j = 0
+        lst = deque()
+        ans = []
+
+        while j < len(nums):
+            while lst and lst[-1] < nums[j]:
+                lst.pop()
+            lst.append(nums[j])
+
+            if j - i + 1 < k:
+                j += 1
+            
+            elif j - i + 1 == k:
+                ans.append(lst[0])
+                if nums[i] == lst[0]:
+                    lst.popleft()
+                i += 1
+                j += 1
+        return ans

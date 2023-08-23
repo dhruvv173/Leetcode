50475939d9e8fdf90806f48aa8358760d25507a5
Dhruv <79097544+dhruvv173@users.noreply.github.com>
2023-08-23 09:31:01 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.py
+++ Subset Sum Problem - GFG/subset-sum-problem.py
@@ -5,18 +5,19 @@ class Solution:
         # code here 
         dp = [[0 for j in range(sum+1)] for i in range(N+1)]
         
-        for i in range(N+1):
-            for j in range(sum+1):
-                if j > 0 and i == 0:
-                    dp[i][sum] = False
-                elif j == 0:
-                    dp[i][j] = True
-                elif arr[i-1] <= j:
-                    dp[i][j]=dp[i-1][j] or dp[i-1][j-arr[i-1]]
+        # setting the row and cols to T or F
+        for i in range(sum+1):
+            dp[0][i] = False
+        for j in range(N+1):
+            dp[j][0] = True
+            
+        for i in range(1, N+1):
+            for j in range(1, sum+1):
+                if arr[i-1] <= j:
+                    dp[i][j] = dp[i-1][j-arr[i-1]] or dp[i-1][j];
                 else:
-                    dp[i][j]=dp[i-1][j]
-        return dp[N][sum]   
-
+                    dp[i][j] = dp[i-1][j];
+        return dp[N][sum]
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
